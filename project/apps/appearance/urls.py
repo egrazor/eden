866dfc03cbce694f49e97d7b8276cd5f5ee246ec
Egor <egor.zenkin36@gmail.com>
2020-04-29 16:52:15 +0300

    вынес логику визуала в app appearance

--- project/apps/appearance/urls.py
+++ project/apps/appearance/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'appearance'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('personal/', views.personal, name='personal'),
 ]
--- project/apps/appearance/views.py
+++ project/apps/appearance/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 
+from cources.models import Faculty, Department, Direction
+from users.models import EdenUser
+
 
 def index(request):
+    faculties = Faculty.objects.all()
+    departments = Department.objects.all()
+    directions = Direction.objects.all()
+    context = {
+        'faculties':  faculties,
+        'departments': departments,
+        'directions': directions
+    }
     template_name = 'appearance/index.html'
-    return render(request, template_name)
+    return render(request, template_name, context)
+
+
+def personal(request):
+    template_name = 'appearance/personal_page.html'
+
+    if request.user.is_authenticated:
+        user = request.user
+    else:
+        raise Http404
+
+    context = {
+        'user': user,
+    }
+    return render(request, template_name, context)
+
